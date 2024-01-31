6835f7a0c2f4a29caefd580a493c1f1cc11f33d1
mehrabix <a.mehrabi@demisco.com>
2024-01-31 16:47:54 +0330

    swigeer

--- src/main/java/com/mehrabi/mehrabi/MehrabiApplication.java
+++ src/main/java/com/mehrabi/mehrabi/MehrabiApplication.java
@@ -6,11 +6,14 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+
 
 
 @SpringBootApplication
 @EntityScan(basePackages = "com.mehrabi.mehrabi")
 @ComponentScan
+@OpenAPIDefinition
 @EnableJpaRepositories
 public class MehrabiApplication {
     public static void main(String[] args) {
