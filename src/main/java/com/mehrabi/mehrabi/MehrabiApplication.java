5ce0cc4ecc7d326a8ab0dbe9f72e415748f94ef2
mehrabix <mehrabi@post.com>
2024-04-12 00:25:08 +0330

    feat(allow all cors): cors origin configuration

--- src/main/java/com/mehrabi/mehrabi/MehrabiApplication.java
+++ src/main/java/com/mehrabi/mehrabi/MehrabiApplication.java
@@ -4,7 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 
@@ -18,5 +21,17 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 public class MehrabiApplication {
     public static void main(String[] args) {
         SpringApplication.run(MehrabiApplication.class, args);
+    }
+
+
+    @Configuration
+    public class WebConfig implements WebMvcConfigurer {
+        @Override
+        public void addCorsMappings(@SuppressWarnings("null") CorsRegistry registry) {
+            registry.addMapping("/**")
+                    .allowedOrigins("http://localhost:3000")
+                    .allowedMethods("GET", "POST", "PUT", "DELETE")
+                    .allowCredentials(true);
+        }
     }
 }
